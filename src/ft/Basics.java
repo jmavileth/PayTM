830b5077edad1ae00ce10c8cdfc7a8cf857c2a48
John-PC <John-PC@DESKTOP-C30CHQT>
2020-07-16 00:51:55 +0530

    This is Design of PayTM business Scenario for Release 1.

---
+++ src/ft/Basics.java
@@ -0,0 +1,19 @@
+package ft;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Basics {
+public static void main(String[] args) throws InterruptedException 
+{
+
+	
+	
+	
+}
+}
